af0e078ae06cf8cb1f5ba62bbcdf09a869e4dbbf
cygnu <crucia8@icloud.com>
2022-02-16 21:06:59 +0900

    feat: Define variables in .tfvars and inject them

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 variable aws_region {}
 
+variable aws_vpc_name {}
+
+variable aws_vpc_cidr {}
+
+variable aws_vpc_azs {}
+
+variable aws_vpc_private_subnets {}
+
+variable aws_vpc_public_subnets {}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -7,12 +17,12 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  name = var.aws_vpc_name
+  cidr = var.aws_vpc_cidr
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.aws_vpc_azs
+  private_subnets = var.aws_vpc_private_subnets
+  public_subnets  = var.aws_vpc_public_subnets
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
