cc7720c88d0cdcb41d4c486a3ee6f651804d1bd4
cygnu <crucia8@icloud.com>
2022-02-16 21:27:43 +0900

    feat: Modularize main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-variable aws_region {}
-
-variable aws_vpc_name {}
-
-variable aws_vpc_cidr {}
-
-variable aws_vpc_azs {}
-
-variable aws_vpc_private_subnets {}
-
-variable aws_vpc_public_subnets {}
-
 provider "aws" {
   region = var.aws_region
 }
