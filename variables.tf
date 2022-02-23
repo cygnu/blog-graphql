ee8dda7f479aad0120c19d4ee3dfb9d3defc037e
cygnu <crucia8@icloud.com>
2022-02-23 22:12:17 +0900

    refactor: Add a comment in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
+# global
 variable "aws_region" {}
 variable "aws_profile" {}
 variable "name_prefix" {}
+
+# tags
 variable "tag_name" {}
+
+# vpc
 variable "aws_vpc_cidr" {}
 variable "aws_vpc_private_subnets" {}
 variable "aws_vpc_public_subnets" {}
